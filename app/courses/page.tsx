aebcda708eed1d0b012a7a73aa636291ca4508cf
Bruno Marques Gurgel <bmg1612@gmail.coml>
2025-01-16 21:26:43 -0300

    chore: fixes bugs and improves pages

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -3,20 +3,27 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { getCoursesDTO } from "@/data/courses-dto";
 import CoursesDraggable from "./courses-draggable";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Courses() {
   const { current, next, read } = await getCoursesDTO();
 
   return (
-    <div className="container mx-auto py-12 px-4 md:px-6 lg:px-8">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+    <div className="container mx-auto py-12 pt-0 px-4 md:px-6 lg:px-8">
+      <div className="flex justify-end items-center">
+        <Button asChild>
+          <Link href="/courses/add">Add Course</Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pt-12">
         <Card>
           <CardHeader>
             <CardTitle>Past Studies</CardTitle>
           </CardHeader>
 
           <CardContent>
-            <ScrollArea className="h-[300px]">
+            <ScrollArea className="h-[500px] overflow-y-auto">
               <div className="grid gap-4">
                 {read.map((item) => (
                   <CourseLine key={item.id} course={item} reverse />
@@ -32,7 +39,9 @@ export default async function Courses() {
           </CardHeader>
 
           <CardContent>
-            <CoursesDraggable courses={current} />
+            <ScrollArea className="h-[500px] overflow-y-auto">
+              <CoursesDraggable courses={current} />
+            </ScrollArea>
           </CardContent>
         </Card>
 
@@ -42,7 +51,9 @@ export default async function Courses() {
           </CardHeader>
 
           <CardContent>
-            <CoursesDraggable courses={next} />
+            <ScrollArea className="h-[500px] overflow-y-auto">
+              <CoursesDraggable courses={next} />
+            </ScrollArea>
           </CardContent>
         </Card>
       </div>
--- app/reading/page.tsx
+++ app/reading/page.tsx
@@ -3,20 +3,27 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getBooksDTO } from "@/data/books-dto";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import ReadingDraggable from "./reading-draggable";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Books() {
   const { current, next, read } = await getBooksDTO();
 
   return (
-    <div className="container mx-auto py-12 px-4 md:px-6 lg:px-8">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+    <div className="container mx-auto py-12 pt-0 px-4 md:px-6 lg:px-8">
+      <div className="flex justify-end items-center">
+        <Button asChild>
+          <Link href="/reading/add">Add Book</Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pt-12">
         <Card>
           <CardHeader>
             <CardTitle>Past Books</CardTitle>
           </CardHeader>
 
-          <CardContent>
-            <ScrollArea className="h-[300px]">
+          <CardContent className="h-full">
+            <ScrollArea className="h-[500px] overflow-y-auto">
               <div className="grid gap-4">
                 {read.map((item) => (
                   <BookLine key={item.id} book={item} reverse />
@@ -32,7 +39,9 @@ export default async function Books() {
           </CardHeader>
 
           <CardContent>
-            <ReadingDraggable books={current} />
+            <ScrollArea className="h-[500px] overflow-y-auto">
+              <ReadingDraggable books={current} />
+            </ScrollArea>
           </CardContent>
         </Card>
 
@@ -42,7 +51,9 @@ export default async function Books() {
           </CardHeader>
 
           <CardContent>
-            <ReadingDraggable books={next} />
+            <ScrollArea className="h-[500px] overflow-y-auto">
+              <ReadingDraggable books={next} />
+            </ScrollArea>
           </CardContent>
         </Card>
       </div>
